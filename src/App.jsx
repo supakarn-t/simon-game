074c3ca17f3e1a17a429150948093b65bb7de6ed
supakarn-t <hikaru8889@gmail.com>
2024-06-29 22:27:06 +0700

    almost finish

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,113 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useEffect } from "react";
 
-function App() {
-  const [count, setCount] = useState(0)
+export default function App() {
+	const [isBlink, setIsBlink] = useState(false);
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+	const [num, setNum] = useState(null);
+
+	const [arr, setArr] = useState([]);
+	const [userArr, setUserArr] = useState([]);
+
+	const [level, setLevel] = useState(2);
+	const [message, setMessage] = useState("Level 1");
+
+	function randomNum() {
+		let newNum = Math.floor(Math.random() * 4) + 1;
+		setNum(newNum);
+		setArr((prev) => [...prev, newNum]);
+		setUserArr([]);
+	}
+
+	function handleClick(e) {
+		const { value } = e.target;
+		setUserArr((prev) => [...prev, Number(value)]);
+
+		console.log(arr);
+		console.log(userArr);
+	}
+
+	useEffect(() => {
+		randomNum();
+
+		setIsBlink(true);
+		setTimeout(() => {
+			setIsBlink(false);
+		}, 1000);
+	}, []);
+
+	useEffect(() => {
+		for (let i = 0; i < userArr.length; i++) {
+			if (arr[i] !== userArr[i]) {
+				setMessage("game over");
+				return;
+			}
+		}
+
+		if (arr.length === userArr.length && userArr.length !== 0) {
+			randomNum();
+
+			setIsBlink(true);
+			setTimeout(() => {
+				setIsBlink(false);
+			}, 500);
+
+			setMessage("Level " + level);
+			setLevel((prev) => prev + 1);
+		}
+	}, [userArr]);
+
+	return (
+		<div className="flex flex-col justify-center items-center gap-16 mt-16">
+			<h1>{message}</h1>
+			<main className="grid grid-cols-2 gap-4 w-1/4">
+				<button
+					value={1}
+					className={`p-4 rounded-lg hover:shadow-md ${
+						message === "game over" ? "" : "active:bg-sky-500"
+					} ${num === 1 && isBlink ? "bg-red-200" : "bg-sky-200"}`}
+					disabled={message === "game over" ? true : false}
+					onClick={handleClick}
+				>
+					1
+				</button>
+				<button
+					value={2}
+					className={`p-4 rounded-lg hover:shadow-md ${
+						message === "game over" ? "" : "active:bg-sky-500"
+					} ${num === 2 && isBlink ? "bg-red-200" : "bg-sky-200"}`}
+					disabled={message === "game over" ? true : false}
+					onClick={handleClick}
+				>
+					2
+				</button>
+				<button
+					value={3}
+					className={`p-4 rounded-lg hover:shadow-md ${
+						message === "game over" ? "" : "active:bg-sky-500"
+					} ${num === 3 && isBlink ? "bg-red-200" : "bg-sky-200"}`}
+					disabled={message === "game over" ? true : false}
+					onClick={handleClick}
+				>
+					3
+				</button>
+				<button
+					value={4}
+					className={`p-4 rounded-lg hover:shadow-md ${
+						message === "game over" ? "" : "active:bg-sky-500"
+					} ${num === 4 && isBlink ? "bg-red-200" : "bg-sky-200"}`}
+					disabled={message === "game over" ? true : false}
+					onClick={handleClick}
+				>
+					4
+				</button>
+				{message === "game over" ? (
+					<button className="col-span-2 p-4 rounded-lg bg-red-200 w-1/2 mx-auto mt-4">
+						Restart
+					</button>
+				) : (
+					""
+				)}
+			</main>
+		</div>
+	);
 }
-
-export default App
